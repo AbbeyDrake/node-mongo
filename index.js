efd9a5b67542a1b268a733bc97b380b47b857de3
Abbey Drake <drakeam1189@gmail.com>
2022-04-04 18:21:28 -0600

    Node MongoDB Example 1

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient; //required mongodb nodeJS driver
 const assert = require('assert').strict;
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
+//use MongoClient object's connect method to make connection to MongoDB server. Callback function gave us client object that we use to access nucampsite database
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 
     assert.strictEqual(err, null);
 
-    console.log('Connected correctly to server');
+    console.log('Connected correctly to server'); //mongodb database server
 
     const db = client.db(dbname);
 
+    //drop and recreate campsite collection from database
     db.dropCollection('campsites', (err, result) => {
         assert.strictEqual(err, null);
         console.log('Dropped Collection', result);
@@ -22,7 +24,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
         (err, result) => {
             assert.strictEqual(err, null);
             console.log('Insert Document:', result.ops);
-
+//console log all documents from campsites collection
             collection.find().toArray((err, docs) => {
                 assert.strictEqual(err, null);
                 console.log('Found Documents:', docs);
